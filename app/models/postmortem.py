61b941252b4ec9358b9a4b76176b8badead63c3f
Mehrdad <mehrdad.pc@gmail.com>
2025-05-22 18:29:50 +0330

    Updaet models

--- app/models/postmortem.py
+++ app/models/postmortem.py
@@ -17,45 +17,110 @@ from app.models.incident import Incident
 
 # Postmortem and its parts
 class PostMortem(BaseEntityID, BaseIncidentEntity, table=True):
-    incident: Annotated["Incident", Relationship(back_populates="postmortem")]
-    approvals: Annotated[List["Approval"],
-                         Relationship(back_populates="postmortem")]
-    action_items: Annotated[List["ActionItem"],
-                            Relationship(back_populates="postmortem")]
-    metadata: Annotated["PostmortemMetadata" | None, Relationship(
-        back_populates="postmortem", sa_relationship_kwargs={"uselist": False})]
-    deep_rca: Annotated["DeepRCA" | None, Relationship(
-        back_populates="postmortem", sa_relationship_kwargs={"uselist": False})]
-    contributing_factors: Annotated[List["ContributingFactor"], Relationship(
-        back_populates="postmortem")]
-    lessons_learned: Annotated[List["LessonLearned"],
-                               Relationship(back_populates="postmortem")]
+
+    incident: Annotated[
+        "Incident",
+        Relationship(
+            back_populates="postmortem"
+        )
+    ]
+
+    approvals: Annotated[
+        List["Approval"],
+        Relationship(
+            back_populates="postmortem"
+        )
+    ]
+
+    action_items: Annotated[
+        List["ActionItem"],
+        Relationship(back_populates="postmortem")
+    ]
+
+    metadata: Annotated[
+        "PostmortemMetadata" | None, Relationship(
+            back_populates="postmortem",
+            sa_relationship_kwargs={"uselist": False}
+        )
+    ]
+
+    deep_rca: Annotated[
+        "DeepRCA" | None, Relationship(
+            back_populates="postmortem",
+            sa_relationship_kwargs={"uselist": False}
+        )
+    ]
+
+    contributing_factors: Annotated[
+        List["ContributingFactor"],
+        Relationship(
+            back_populates="postmortem"
+        )
+    ]
+
+    lessons_learned: Annotated[
+        List["LessonLearned"],
+        Relationship(back_populates="postmortem")
+    ]
 
 
 class PostmortemMetadata(BasePostmortemEntity, table=True):
-    postmortem: Annotated[PostMortem, Relationship(back_populates="metadata")]
+
+    postmortem: Annotated[
+        PostMortem,
+        Relationship(back_populates="metadata")
+    ]
 
 
 class DeepRCA(BasePostmortemEntity, table=True):
-    postmortem: Annotated[PostMortem, Relationship(back_populates="deep_rca")]
+
+    postmortem: Annotated[
+        PostMortem,
+        Relationship(back_populates="deep_rca")
+    ]
 
 
 class ContributingFactor(BasePostmortemEntity, table=True):
-    postmortem: Annotated[PostMortem, Relationship(
-        back_populates="contributing_factors")]
+
+    postmortem: Annotated[
+        PostMortem, Relationship(
+            back_populates="contributing_factors"
+        )
+    ]
 
 
 class LessonLearned(BasePostmortemEntity, table=True):
-    postmortem: Annotated[PostMortem, Relationship(
-        back_populates="lessons_learned")]
+
+    postmortem: Annotated[
+        PostMortem, Relationship(
+            back_populates="lessons_learned"
+        )
+    ]
 
 
 # Action items
 class ActionItem(BasePostmortemEntity, table=True):
+
     task: str
-    owner_id: Annotated[UUID, Field(foreign_key="user.id")]
-    due_date: Annotated[date, Field(default_factory=date.today)]
+
     status: StatusEnum
-    postmortem: Annotated[PostMortem, Relationship(
-        back_populates="action_items")]
-    owner_user: Annotated["User", Relationship(back_populates="action_items")]
+
+    owner_id: Annotated[
+        UUID,
+        Field(foreign_key="user.id")
+    ]
+
+    due_date: Annotated[
+        date,
+        Field(default_factory=date.today)
+    ]
+
+    postmortem: Annotated[
+        PostMortem,
+        Relationship(back_populates="action_items")
+    ]
+
+    owner_user: Annotated[
+        "User",
+        Relationship(back_populates="action_items")
+    ]
