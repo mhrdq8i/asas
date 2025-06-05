9ee86ab2f8d332a7193fcba49ae3f030b7907bdf
Mehrdad <mehrdad.pc@gmail.com>
2025-06-05 19:54:54 +0330

    Improve models import rules

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -1,7 +1,24 @@
+"""
+This file imports all SQLModel table models and Base models,
+ensuring they are registered with SQLModel's metadata.
+It also runs model_rebuild() for models with forward references.
+This __init__.py should be imported once when the application starts,
+for example in src/database/session.py before creating tables.
+"""
+
+from src.models.base import (
+    IncidentStatusEnum,
+    SeverityLevelEnum,
+    ActionItemStatusEnum,
+    UserRoleEnum,
+    PostMortemApproverRoleEnum,
+    BaseEntity,
+)
 from src.models.user import User
 from src.models.incident import (
     Incident,
     IncidentProfile,
+    AffectedItemBase,
     AffectedService,
     AffectedRegion,
     Impacts,
@@ -42,3 +59,49 @@ ContributingFactor.model_rebuild()
 LessonLearned.model_rebuild()
 ActionItem.model_rebuild()
 PostMortemApproval.model_rebuild()
+
+
+print(
+    "[INFO] "
+    "All models from 'src.models' "
+    "package imported and forward "
+    "references updated."
+)
+
+# Define __all__ to control
+# `from src.models import *` behavior
+__all__ = [
+    # Bases and Enums
+    "BaseEntity",
+    "AffectedItemBase",
+    "IncidentStatusEnum",
+    "SeverityLevelEnum",
+    "ActionItemStatusEnum",
+    "UserRoleEnum",
+    "PostMortemApproverRoleEnum",
+
+    # User Models
+    "User",
+
+    # Incident Models
+    "Incident",
+    "IncidentProfile",
+    "AffectedService",
+    "AffectedRegion",
+    "Impacts",
+    "ShallowRCA",
+    "TimelineEvent",
+    "ResolutionMitigation",
+    "RemediationStep",
+    "LongTermPreventativeMeasure",
+    "CommunicationLogEntry",
+    "SignOff",
+
+    # PostMortem Models
+    "PostMortem",
+    "PostMortemProfile",
+    "ContributingFactor",
+    "LessonLearned",
+    "ActionItem",
+    "PostMortemApproval",
+]
