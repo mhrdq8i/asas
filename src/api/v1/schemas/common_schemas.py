884e852fa219b413ceb919e7a8a235516b3e60b8
Mehrdad <mehrdad.pc@gmail.com>
2025-07-01 15:56:47 +0330

    Beautify common_schemas

--- src/api/v1/schemas/common_schemas.py
+++ src/api/v1/schemas/common_schemas.py
@@ -1,10 +1,14 @@
-from typing import Generic, TypeVar, List
 from pydantic import BaseModel
+from typing import Generic, TypeVar, List
+
 
 T = TypeVar("T")
 
 
-class PaginatedResponse(BaseModel, Generic[T]):
+class PaginatedResponse(
+    BaseModel,
+    Generic[T]
+):
     items: List[T]
     total: int
     skip: int
